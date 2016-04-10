bad0a68a81edaeff5801c6842dbcad6c925e6c31
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-04-10 20:19:10 +0800

    update function doc
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/coldnew/config.clj
+++ src/coldnew/config.clj
@@ -8,21 +8,41 @@
   []
   (reset! impl/*enable-eval* true))
 
-(defonce ^{:doc "A map of environment variables."}
-  env
-  ;; Environ redolved config in following order
-  ;; 1. .lein-env file in the project directory
-  ;; 2. .boot-env file in the project directory
-  ;; 3. Environment variables
-  ;; 4. Java system properties
-  environ/env)
+(defonce ^{:doc
+           "A map of configuration and environment variables.
+This variable is resolved in the following order, the variables found in later weill replace tholse declared eariler:
 
-(defonce ^{:doc "A map of configuration and environment variables."}
-  config
+1. `config.edn` on the classpath.
+2. EDN file specified using the `config` environment variable.
+3. `.lein-env` file in the project directory.
+4. `.boot-env` file in the project directory.
+5. Environment vairables.
+6. Java system propertirs."
+           :doc/format :markdown}
+  env
   (merge
-   ;; 1. find config.edn in classpath
+   ;; 1. Find config.edn in classpath
    (impl/read-resource "config.edn")
    ;; 2. If environment variable `CONFIG' is specified, also read it
    (impl/read-file (environ.core/env :config))
-   ;; 3. Find info from env
-   env))
+   ;; 3. Find info fron env
+   environ/env))
+
+(defonce ^{:doc "A map of environment and configuration variables.
+This variable is resolved in the following order, the variables found in later weill replace tholse declared eariler:
+
+1. `.lein-env` file in the project directory.
+2. `.boot-env` file in the project directory.
+3. Environment vairables.
+4. Java system propertirs.
+5. `config.edn` on the classpath.
+6. EDN file specified using the `config` environment variable."
+           :doc/format :markdown}
+  config
+  (merge
+   ;; 1. Find info from env
+   environ.core/env
+   ;; 2. Find config.edn in classpath
+   (impl/read-resource "config.edn")
+   ;; 3. If environment variable `CONFIG' is specified, also read it
+   (impl/read-file (environ.core/env :config))))
